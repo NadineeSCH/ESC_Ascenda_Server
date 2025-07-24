4fa7e83f2de7c7c37f165fbaa634cdfbebeb94d3
NadineVM <155554497+NadineeSCH@users.noreply.github.com>
2025-07-24 21:40:15 +0800

    feat: cleaned original json, filter out only relevant info

--- routes/hoteldetails.js
+++ routes/hoteldetails.js
@@ -16,8 +16,28 @@ router.post("/", async function (req, res, next) {
   const targetUrl = `https://hotelapi.loyalty.dev/api/hotels/${req.body.hotelId}/price?destination_id=${req.body.destinationId}&checkin=${req.body.checkin}&checkout=${req.body.checkout}&lang=en_US&currency=${req.body.currency}&country_code=SG&guests=${guests}&partner_id=1`;
 
   let data = await poller(targetUrl);
+  let cleanedJson = [];
 
-  res.json(data.rooms);
+  for (const room of data.rooms) {
+    const cleanedRoom = {
+      roomNormalizedDescription: room.roomNormalizedDescription,
+      free_cancellation: room.free_cancellation,
+      breakfastInfo: room.roomAdditionalInfo?.breakfastInfo, // Using optional chaining
+      additionalInfo: room.roomAdditionalInfo,
+      longDesc: room.long_description, 
+      price: room.price,
+    };
+
+    // Find hero image
+    const heroImage = room.images?.find((image) => image.hero_image === true);
+    if (heroImage) {
+      cleanedRoom.image = heroImage.url;
+    }
+
+    cleanedJson.push(cleanedRoom);
+  }
+
+  res.json(cleanedJson);
 });
 
 module.exports = router;
