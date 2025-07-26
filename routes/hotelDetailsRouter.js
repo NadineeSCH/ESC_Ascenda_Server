3860bc1d48794fd70f1996d6a3e4a68c12d36667
Srikanth Srider <165422078+SrikanthSrider@users.noreply.github.com>
2025-07-27 06:10:24 +0800

    start clean slate

--- routes/hotelDetailsRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-var router = express.Router();
-const hotelDetailsController = require("../controller/hotelDetailsController")
-
-router.post("/", hotelDetailsController.getHotelDetails);
-
-module.exports = router;
