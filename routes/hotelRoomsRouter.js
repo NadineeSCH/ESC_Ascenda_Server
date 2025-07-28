a421064d1bd9b5b9d47a9dd94a7576dc725d9196
michaeljr <michael.rasco24@gmail.com>
2025-07-28 11:07:22 +0800

    attempting merge

--- routes/hotelRoomsRouter.js
+++ routes/hotelRoomsRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 var router = express.Router();
 const hotelRoomsController = require("../controller/hotelRoomsController")
 
+console.log("in hotelroomsrouter");
+
 router.post("/", hotelRoomsController.getRoomDetails);
 
 module.exports = router;
