c2f33f03e9971e83e3a7e3909627a49fda800341
Ng Ding jie <122772464+Notmatcha@users.noreply.github.com>
2025-08-09 16:41:41 +0800

    Update profRoutes.js
    added des to make the call more easier

--- routes/profRoutes.js
+++ routes/profRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const profController = require('../controller/profController');
+const profController = require('../controllers/profController');
 const authenticateToken = require('../middleware/authMiddleware');
 
 // Profile routes
-router.get('/', authenticateToken, profController.getProfile);
-router.patch('/', authenticateToken, profController.updateProfile);
-router.delete('/', authenticateToken, profController.deleteAccount);
+router.get('/get', authenticateToken, profController.getProfile);
+router.patch('/patch', authenticateToken, profController.updateProfile);
+router.delete('/delete', authenticateToken, profController.deleteAccount);
 
 // Purchase history
 router.get('/history', authenticateToken, profController.getPurchaseHistory);
